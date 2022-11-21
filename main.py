afaef08f46427f8f0755fcc3cdf779b2d825766b
Daniel-Fundarek <xfundarekd@stuba.sk>
2022-11-21 22:22:01 +0100

    connect, takeoff , land, flip implemented
    Simplified version of GUI that provides basic functionality like take off landing  etc by pushoing button. Each command has its own thread, so it doesnt freeze main window

--- main.py
+++ main.py
@@ -10,40 +10,13 @@ from window import FlightState
 from window import DroneCommand
 
 
-async def main():
+def main():
     window = Window()
-    drone = Tello()
-    state = FlightState.IDLE
-    command = DroneCommand.IDLE
-    while state != FlightState.DISCONNECTED:
-        state = window.get_state()
-        command = window.get_command()
 
-        if state == FlightState.IDLE and command == DroneCommand.CONNECT:
-            await drone.connect()
-            state = FlightState.CONNECTED
-        if state == FlightState.CONNECTED and command == DroneCommand.DISCONNECT:
-            await drone.disconnect()
-            state = FlightState.DISCONNECTED
-
-        if state == FlightState.CONNECTED and command == DroneCommand.TAKEOFF:
-            await drone.takeoff()
-            state = FlightState.FLYING
-
-        if state == FlightState.FLYING and command == DroneCommand.LAND:
-            await drone.land()
-            state = FlightState.CONNECTED
-
-        if state == FlightState.FLYING and command == DroneCommand.FLIP:
-            await drone.flip_forward()
-
-        command = DroneCommand.IDLE
-        window.set_state(command)
-        window.set_command(command)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- window.py
+++ window.py
@@ -1,6 +1,9 @@
 import tkinter
-
+import asyncio
+from djitellopy import Tello
+# from tello_asyncio import Tello
 from enum import Enum
+import threading
 
 
 class FlightState(Enum):
@@ -27,60 +30,66 @@ class Window:
         self.window.geometry('500x500')
         self.command = DroneCommand.IDLE
         self.state = FlightState.IDLE
+        self.drone = Tello()
 
-        self.button1 = tkinter.Button(self.window, text=DroneCommand.TAKEOFF, command=lambda: self.onclick(1))
+        self.button1 = tkinter.Button(self.window, text=DroneCommand.TAKEOFF, command=lambda: self.start_onclick_thread(1))
         self.button1.grid(row=1, column=0)
 
-        self.button2 = tkinter.Button(self.window, text=DroneCommand.FLIP, command=lambda: self.onclick(2))
+        self.button2 = tkinter.Button(self.window, text=DroneCommand.FLIP, command=lambda: self.start_onclick_thread(2))
         self.button2.grid(row=2, column=0)
 
-        self.button3 = tkinter.Button(self.window, text=DroneCommand.CONNECT, command=lambda: self.onclick(3))
+        self.button3 = tkinter.Button(self.window, text=DroneCommand.CONNECT, command=lambda: self.start_onclick_thread(3))
         self.button3.grid(row=3, column=0)
+        self.button1 = tkinter.Button(self.window, text=DroneCommand.LAND, command=lambda: self.start_onclick_thread(4))
+        self.button1.grid(row=1, column=1)
+
+        self.button2 = tkinter.Button(self.window, text='Battery status', command=lambda: self.start_onclick_thread(5))
+        self.button2.grid(row=2, column=1)
+
         self.window.mainloop()
 
     def set_name_button(self, text):
         self.button1.config(text=text)
 
+    def start_onclick_thread(self, num):
+        threading.Thread(target=lambda: self.onclick(num)).start()
+
     def onclick(self, button_num):
 
         if button_num == 1:
-            if self.button1['text'] == DroneCommand.TAKEOFF:
-                if self.state == FlightState.CONNECTED:
-                    self.button1['text'] = DroneCommand.LAND
-                    self.command = DroneCommand.TAKEOFF
-            else:
-                if self.state.FLYING:
-                    self.button1['text'] = DroneCommand.TAKEOFF
-                    self.command = DroneCommand.LAND
+
+            self.drone.takeoff()
+            # self.takeoff(True)
 
         elif button_num == 2:
-            if self.state == FlightState.FLYING:
-                self.command = DroneCommand.FLIP
+            # self.drone.flip_back()
 
+            self.drone.flip_right()
+
+            # self.flip()
 
         elif button_num == 3:
-            if self.state == FlightState.IDLE:
-                self.command = DroneCommand.CONNECT
-                self.button3['text'] = DroneCommand.DISCONNECT
-                return
-            if self.state == FlightState.CONNECTED:
-                self.command = DroneCommand.DISCONNECT
-                self.button3['text'] = DroneCommand.CONNECT
 
+            self.drone.connect()
+            # self.connect(True)
 
+        elif button_num == 4:
+            self.drone.land()
+            # self.takeoff(False)
 
+        elif button_num == 5:
+            battery = self.drone.get_battery()
+            print(f'battery percentage: {battery}%')
+        # self.disconnect(False)
 
     def set_state(self, state):
         self.state = state
 
-
     def get_state(self):
         return self.state
 
-
     def get_command(self):
         return self.command
-
 
     def set_command(self, command):
         self.command = command
